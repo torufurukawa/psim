dec336f575c04e7d10082cff5d9ccc025aa832dd
Toru Furukawa <torufurukawa@gmail.com>
2017-03-20 14:34:04 +0900

    Add test

--- syncman/syncman.go
+++ syncman/syncman.go
@@ -1,7 +1,11 @@
 package syncman
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"log"
+	"net"
 	"os"
 )
 
@@ -30,11 +34,41 @@ type settings struct {
 	ConfigPath string
 }
 
-type syncManager struct{}
+type syncManager struct {
+	settings *settings
+}
 
 func newSyncManager(s *settings) *syncManager {
-	sm := syncManager{}
+	sm := syncManager{settings: s}
 	return &sm
 }
 
-func (sm *syncManager) Run() {}
+func (sm *syncManager) Run() error {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", sm.settings.Port))
+	if err != nil {
+		return err
+	}
+
+	for {
+		rw, err := ln.Accept()
+		// TODO design and test error handling
+		if err != nil {
+			log.Println(err)
+		}
+
+		// handle
+		scanner := bufio.NewScanner(rw)
+		ok := scanner.Scan()
+		if !ok {
+			continue
+		}
+		line := scanner.Text()
+		var resp string
+		if line == "+PING\r\n" {
+			resp = "+OK\r\n"
+		} else {
+			resp = "-Unknown command: "
+		}
+		_ = resp
+	}
+}
